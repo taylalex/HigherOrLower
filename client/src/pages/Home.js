e8e9d6bbd2a66af5156d861cd210fe1cf9aa45a6
Taylor Stubbs <taylor-stubbs@live.co.uk>
2022-09-13 10:25:22 +0100

    Created template for main page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,35 @@
+import styled from 'styled-components'
+import logo from '../assets/logo.png'
+
+const Header = styled.div`
+    margin: auto;
+    padding: 10px;
+`
+
+const Footer = styled.div`
+    background-color:#252627;
+    color: white;
+    font-family: Arial;
+    padding: 10px;
+    text-align: center;
+`
+const Logo = styled.img`
+    display: block;
+    margin: auto;
+    width: 20%;
+    min-width: 200px;
+`
+
 function Home() {
     return (
-        <p>hello!</p>
+        <>
+            <Header>
+                <Logo src={logo}/>
+            </Header>
+            <Footer>
+                Created by Taylor Stubbs
+            </Footer>
+        </>
     )
 }
 
